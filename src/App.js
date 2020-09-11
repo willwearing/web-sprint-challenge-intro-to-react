6bdd07eba8801f8d2ef2a438318b28474e85c913
willwearing <willwearing@gmail.com>
2020-09-11 09:45:52 -0600

    found data using star wars api and have saved to starChars

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [chars, setChars] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        console.log(response);
+        const starChars = response.data.results;
+        console.log(starChars);
+      })
+      .catch((error) => {
+        console.log("Sorry, no Characters", error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
